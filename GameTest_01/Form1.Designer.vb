8dcb3eec1c4adab3892b9fb20d127753904e14bd
PrCjKC <pcasey@infoqwest.net>
2018-06-21 23:27:48 -0400

    This is my first attempt at posting to Git. This si a basic example of a games base code in Winforms using VB

---
+++ GameTest_01/Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pbSurface = New System.Windows.Forms.PictureBox()
+        CType(Me.pbSurface, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbSurface
+        '
+        Me.pbSurface.BackColor = System.Drawing.Color.Black
+        Me.pbSurface.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pbSurface.Location = New System.Drawing.Point(0, 0)
+        Me.pbSurface.Name = "pbSurface"
+        Me.pbSurface.Size = New System.Drawing.Size(284, 261)
+        Me.pbSurface.TabIndex = 0
+        Me.pbSurface.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.pbSurface)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbSurface, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pbSurface As PictureBox
+End Class
---
+++ GameTest_01/Form1.vb
@@ -0,0 +1,113 @@
+﻿Public Class Form1
+    Dim timer As Stopwatch
+    Dim backBuffer As Image
+    Dim graphics As Graphics
+    Dim clientWidth As Integer
+    Dim clientHeight As Integer
+    Dim interval As Long
+    Dim startTick As Long
+    Dim imageRect As Rectangle
+    Dim direction As Point
+    Dim player As Rectangle
+    Dim playerDirection As Point
+    Dim playSprite As Bitmap
+    Private playerSpeed As Integer = 5
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        GameLoop()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.DoubleBuffered = True
+        Me.MaximizeBox = False
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+        timer = New Stopwatch
+        clientHeight = 300
+        clientWidth = 300
+        interval = 16
+        Me.ClientSize = New Size(clientWidth, clientHeight)
+        backBuffer = New Bitmap(clientWidth, clientHeight)
+        graphics = Graphics.FromImage(backBuffer)
+        direction = New Point(2, 3)
+        imageRect = New Rectangle(0, 0, 30, 30)
+        player = New Rectangle(1, 1, 1, 1)
+        playSprite = New Bitmap(My.Resources.SpaceShip)
+    End Sub
+
+    Private Sub GameLoop()
+        timer.Start()
+        Do While (Me.Created)
+            startTick = timer.ElapsedMilliseconds
+            GameLogic()
+            RenderScene()
+            Application.DoEvents()
+            Do While timer.ElapsedMilliseconds - startTick < interval
+
+            Loop
+        Loop
+    End Sub
+
+    Private Sub GameLogic()
+        imageRect.X += direction.X
+        imageRect.Y += direction.Y
+
+        If imageRect.X < 0 Then
+            imageRect.X = 3
+            direction.X *= -1
+        End If
+        If imageRect.Y < 0 Then
+            imageRect.Y = 3
+            direction.Y *= -1
+        End If
+        If imageRect.X + imageRect.Width > clientWidth Then
+            imageRect.X = clientWidth - imageRect.Width
+            direction.X *= -1
+        End If
+        If imageRect.Y + imageRect.Height > clientHeight Then
+            imageRect.Y = clientHeight - imageRect.Height
+            direction.Y *= -1
+        End If
+    End Sub
+
+    Private Sub RenderScene()
+        backBuffer = New Bitmap(clientWidth, clientHeight)
+        graphics = Graphics.FromImage(backBuffer)
+        pbSurface.Image = Nothing
+        graphics.FillRectangle(Brushes.Blue, imageRect)
+        'graphics.FillRectangle(Brushes.Green, player)
+        graphics.DrawImage(playSprite, player.Location)
+        pbSurface.Image = backBuffer
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        player.X += playerDirection.X
+        player.Y += playerDirection.Y
+
+        Select Case e.KeyCode
+            Case Keys.Up, Keys.W
+                If Not player.Y < 0 Then
+                    player.Y -= playerSpeed
+                Else
+                    player.Y = 3
+                End If
+            Case Keys.Down, Keys.S
+                If player.Y + player.Height < clientHeight Then
+                    player.Y += playerSpeed
+                Else
+                    player.Y = clientHeight - player.Height - 3
+                End If
+            Case Keys.Left, Keys.A
+                If player.X > 0 Then
+                    player.X -= playerSpeed
+                Else
+                    player.X = 3
+                End If
+            Case Keys.Right, Keys.D
+                If player.X + player.Width < clientWidth Then
+                    player.X += playerSpeed
+                Else
+                    player.X = clientWidth - player.Width - 3
+                End If
+        End Select
+    End Sub
+End Class
